efdb1d694515d63cbfd180b98eedf45ffaf4c790
TMGilchrist <telcontar@hotmail.co.uk>
2018-02-16 11:23:43 +0000

    Rough version implemented. Fractal not quite right yet.

--- Fractal/main.cpp
+++ Fractal/main.cpp
@@ -8,10 +8,12 @@ int main(int, char**)
 {
 	int windowWidth = 800;
 	int windowHeight = 800;
+
+
 	//Initialise the Video Part of SDL2
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) 
 	
-{
+	{
 		//Print out an error message to the screen if this fails
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
@@ -51,6 +53,7 @@ int main(int, char**)
 	// Minimum and maximum coordinates for the fractal
 	const double minX = -2, maxX = 1, minY = -1.5, maxY = 1.5;
 
+
 	//Controls the game loop
 	bool quit = false;
 	//Holds events coming from SDL
@@ -75,37 +78,79 @@ int main(int, char**)
 		//do drawing here
 		SDL_LockTexture(fractalTexture, NULL, (void**)&pixels, &pitch);
 
+		double currentX;
+		double currentY;
+		double nextX;
+		double nextY;
 
 
+		std::cout << "Begin looping over screen pixels";
+
+		//Iterate over all the pixels on the screen
 		for (int pixelY = 0; pixelY < windowHeight; pixelY++) 
 		{
 			// TODO: Map the y coordinate into the range minY to maxY
-			//double y0 =
+			double y0 = ((double)pixelY / (double)windowWidth) * (maxY - minY) + minY;
+
 			for (int pixelX = 0; pixelX < windowWidth; pixelX++)
 			{
-
 				// TODO: Map the x coordinate into the range minX to maxX
-				//double x0 =
+				double x0 = ((double)pixelX / (double)windowWidth) * (maxX - minX) + minX;
+
+				//std::cout << "\n PixelX: " << pixelX << " \n PixelY: " << pixelY;
+				//std::cout << "\n x0, y0: " << x0 << ", " << y0;
+				//std::cin.get();
 
 				unsigned int pixelPosition = pixelY * (pitch / pixelFormat->BytesPerPixel) + pixelX;
 
-				// TODO: implement the algorithm to colour a single pixel (x0, y0) of the fractal
-				// The code below simply fills the screen with random pixels
+				//Initialise X and Y as x0, y0
+				currentX = x0;
+				currentY = y0;
 
-				// Write the pixel
-				// TODO: change this for desired pixel colour value
-				Uint32 colour = SDL_MapRGB(pixelFormat, rand()%255, rand() % 255, rand() % 255);
-				// Now we can set the pixel(s) we want.
-				pixels[pixelPosition] = colour;
+				//Set smallestValue start
+				int smallestValue = 9000;
+
+				//Iterate x0 .. x199 and check if it satisfies equation
+				for (int i = 0; i < 50; i++)
+				{					
+					if (pow(currentX, 2) + pow(currentY, 2) >= 4)
+					{
+						if (i < smallestValue)
+						{
+							smallestValue = i;
+						}
+					}
+
+					//Calculate new X and Y	
+					nextX = pow(currentX, 2) - pow(currentY, 2) + x0;
+					nextY = (2 * currentX * currentY) + y0;	
+
+					currentX = nextX;
+					currentY = nextY;
+				}
+								
+
+				//Define colours
+				Uint32 colour = SDL_MapRGB(pixelFormat, 255, 0, 0);
+				Uint32 black = SDL_MapRGB(pixelFormat, 0, 0, 0);
+
+				//Set pixel colours
+				if (smallestValue == 9000)
+				{
+					pixels[pixelPosition] = black;
+				}
+
+				else
+				{
+					pixels[pixelPosition] = colour;
+				}
 			}
 		}
 
-		SDL_UnlockTexture(fractalTexture);
-		
+		SDL_UnlockTexture(fractalTexture);		
 		SDL_RenderCopy(renderer, fractalTexture, NULL, NULL);
+
 		//Display the work the renderer has been doing, this make something appear on the screen
-
-
 		SDL_RenderPresent(renderer);
 	}
 
